1581aaa524047774328271ccf3617368f169b7b3
alexia <20200992@uaz.edu.mx>
2023-12-05 21:55:29 -0600

    Updates

---
+++ precache-manifest.7d91f0247e1a7807eb8355d62199f999.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f3d4459caa10ef18bb0d132a8b3584",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b2ef15913e848990de3f",
+    "url": "/react-portfolio/static/css/main.108e5fb1.chunk.css"
+  },
+  {
+    "revision": "40024298b26eb0799206",
+    "url": "/react-portfolio/static/js/2.1bedeb2d.chunk.js"
+  },
+  {
+    "revision": "b2ef15913e848990de3f",
+    "url": "/react-portfolio/static/js/main.2fe878ea.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "b622fdc38fb92ffe41ec442614842dcd",
+    "url": "/react-portfolio/static/media/CASE.b622fdc3.png"
+  },
+  {
+    "revision": "3b7cf8a5d44d819c068e9b3d567bfc3f",
+    "url": "/react-portfolio/static/media/Menu.3b7cf8a5.png"
+  },
+  {
+    "revision": "18dec6b0d12f7da91ce2092bcf7d11b7",
+    "url": "/react-portfolio/static/media/bg-texture.18dec6b0.jpg"
+  },
+  {
+    "revision": "afea61d5b5c8bb07d8454152f127be4f",
+    "url": "/react-portfolio/static/media/collectors.afea61d5.png"
+  },
+  {
+    "revision": "5e289bcbeddf773b8573ea69806d1403",
+    "url": "/react-portfolio/static/media/me.5e289bcb.png"
+  },
+  {
+    "revision": "f641bf03aba8c3b20b0f32625232ef32",
+    "url": "/react-portfolio/static/media/me3.f641bf03.png"
+  },
+  {
+    "revision": "c730f48d458518ae291f16730af28ea7",
+    "url": "/react-portfolio/static/media/output.c730f48d.jpg"
+  },
+  {
+    "revision": "53c8ec1259f370492013b51ca6755f8c",
+    "url": "/react-portfolio/static/media/resume.53c8ec12.pdf"
+  }
+]);

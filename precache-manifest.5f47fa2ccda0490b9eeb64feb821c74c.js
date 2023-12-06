43418d28d3c55cc6a9f9b94a4163869c2804ec5c
alexia <20200992@uaz.edu.mx>
2023-12-05 21:46:38 -0600

    Updates

---
+++ precache-manifest.5f47fa2ccda0490b9eeb64feb821c74c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba6db85e7efc0a22befd3c564476d42",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/index.html"
+  },
+  {
+    "revision": "7106b7d67ef3c72b028e",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/css/main.9f55c046.chunk.css"
+  },
+  {
+    "revision": "40024298b26eb0799206",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/js/2.1bedeb2d.chunk.js"
+  },
+  {
+    "revision": "7106b7d67ef3c72b028e",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/js/main.2fe878ea.chunk.js"
+  },
+  {
+    "revision": "23c0b906cda035a8cd9a",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/js/runtime~main.b8383cdb.js"
+  },
+  {
+    "revision": "b622fdc38fb92ffe41ec442614842dcd",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/media/CASE.b622fdc3.png"
+  },
+  {
+    "revision": "3b7cf8a5d44d819c068e9b3d567bfc3f",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/media/Menu.3b7cf8a5.png"
+  },
+  {
+    "revision": "18dec6b0d12f7da91ce2092bcf7d11b7",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/media/bg-texture.18dec6b0.jpg"
+  },
+  {
+    "revision": "afea61d5b5c8bb07d8454152f127be4f",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/media/collectors.afea61d5.png"
+  },
+  {
+    "revision": "5e289bcbeddf773b8573ea69806d1403",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/media/me.5e289bcb.png"
+  },
+  {
+    "revision": "f641bf03aba8c3b20b0f32625232ef32",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/media/me3.f641bf03.png"
+  },
+  {
+    "revision": "c730f48d458518ae291f16730af28ea7",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/media/output.c730f48d.jpg"
+  },
+  {
+    "revision": "53c8ec1259f370492013b51ca6755f8c",
+    "url": "%3Cusername%3E.github.io/%3Crepository-name%3E/static/media/resume.53c8ec12.pdf"
+  }
+]);
